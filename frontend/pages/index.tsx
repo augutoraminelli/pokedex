6e828b01923fab9eb7b94c9e05d74cf0ced31a8b
Augusto <augusto.raminelli>
2022-08-08 15:54:13 -0300

    chore: install antd lib

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import "antd/dist/antd.css";
+
+import { Button } from "antd";
+import { useEffect } from "react";
+import api from "../service/api";
 
 const Home: NextPage = () => {
   return (
@@ -8,10 +13,27 @@ const Home: NextPage = () => {
         <title>Homepage</title>
       </Head>
       <main>
-        <h1>Hello World!</h1>
+        <Button type="primary" shape="circle">
+          Hellor World!
+        </Button>
       </main>
     </div>
-  )
-}
+  );
 
-export default Home
+  // export async function getStaticProps() {
+  //   // Call an external API endpoint to get posts.
+  //   // You can use any data fetching library
+  //   const res = await api("https://.../posts");
+  //   const posts = await res.json();
+
+  //   // By returning { props: { posts } }, the Blog component
+  //   // will receive `posts` as a prop at build time
+  //   return {
+  //     props: {
+  //       posts,
+  //     },
+  //   };
+  // }
+};
+
+export default Home;
