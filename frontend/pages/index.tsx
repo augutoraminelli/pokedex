fb47b316c8bbc9bd606f0be7fc69de59f42ba7c6
Augusto <augusto.raminelli>
2022-08-08 13:52:16 -0300

    chore: install typescript and setup initial

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+      
+      <main>
+        <h1>Hello Wolrd!</h1>
+      </main>
+    </div>
+  )
+}
